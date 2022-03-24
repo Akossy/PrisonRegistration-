d70a11f862873d3d486cca6f971fbb8d381d7ad0
admin <admin@admins-MacBook-Air.local>
2022-03-24 09:54:34 +0000

    initial file

---
+++ src/main/java/com/qa/prisonregister/Dfesw13PrisonRegisterFinalApplication.java
@@ -0,0 +1,13 @@
+package com.qa.prisonregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dfesw13PrisonRegisterFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dfesw13PrisonRegisterFinalApplication.class, args);
+	}
+
+}
